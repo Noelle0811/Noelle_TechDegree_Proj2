9ec0f6ce8c85410e97229e29c0c489a198057f26
Kiel Lewis <kiellewis@Kiels-MacBook-Pro.local>
2019-08-30 10:59:30 -0400

    completed proj2
    List Pagination and Filtering

--- js/script2.js
+++ js/script2.js
@@ -5,36 +5,9 @@ FSJS project 2 - List Filter and Pagination
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
 const studentList = document.querySelectorAll('.student-item');
 const itemsPerPage = 10;
 
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
 
 function showPage(list, page) {
    //created a range of users
@@ -55,11 +28,6 @@ function showPage(list, page) {
 
 console.log(showPage(studentList,1));
 
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
 
 const appendPageLinks = (list) => {// add "list" parameter
 var pageDiv = document.querySelector('.page'); //get parent
@@ -88,66 +56,18 @@ ul.addEventListener('click', (e) => {
    for (let i = 0; i < numberOfPages; i++) {
        ul.children[i].firstElementChild.className = '';
    }
-   if (e.target.tagName === 'a') {
+   if (e.target.tagName === 'A') {
        const page = e.target.textContent;
        // active link click
        e.target.className = 'active';
-
+       //calling everything to the page
        showPage(studentList, page);
        showPage(list, page);
    }
 });
 };
 
-
-
-
 //to load page
 showPage(studentList, 1);
 appendPageLinks(studentList);
 
-var buttonLinks = document.querySelector('a');
-var a = document.querySelector('a');
-
-a.innerHTML= buttonLinks;
-   a.addEventListener('click', (e) => {
-      if(e.target.tagName === 'a'){
-         a.children[0].children[0].classList.remove('active');
-        for(let i=1; i<=numOfPage; i++){
-           if(e.target.innerHTML.includes(i)){
-               showPage(studentList, i);
-           }
-         }
-      }
-   })
-
-
-//the page and name change when you click on the page buttons
-// ul.addEventListener('click', (e) => {
-//     showPage(list, event.target.textContent);
-//     removeClass();
-//       });
-  
-
-
-//appendPageLinks();
-
-
-// for (let i = 1; i < numberOfPages; i += 1) {
-//       const li = document.createElement("li");
-//       ul.appendChild(li);
-//       const a = document.createElement('a');
-//       li.appendChild(a);
-//       a.textContent = i;
-//       a.href = "#";
-//       ul.addEventListener('click'), (e) => {
-//       if (e.target.tagName === ('a')) {
-//       const activeLink = e.target;
-//       activeLink.className = "active";
-//       }
-//    }
-// }
-// }
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
